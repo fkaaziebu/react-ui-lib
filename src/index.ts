2a71bd8826c4134b28af33dfaa53a545e53691ee
fkaaziebu <frederickaziebu1998@gmail.com>
2024-07-09 01:57:32 +0000

    reset project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export {
+    IAuthPayload,
+    IAuthDocument,
+    IAuthBuyerMessageDetails,
+    IEmailMessageDetails,
+    IAuth,
+    ISignUpPayload,
+    ISignInPayload,
+    IForgotPassword,
+    IResetPassword,
+    IReduxAuthPayload,
+    IReduxAddAuthUser,
+    IReduxLogout,
+    IAuthResponse,
+    IAuthUser,
+  } from './auth.interface';
