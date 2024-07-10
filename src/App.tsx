f7f2d7cd827c456247885d8563055d15d54b049b
fkaaziebu <frederickaziebu1998@gmail.com>
2024-07-10 12:42:48 +0000

    feat: building react library with vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// @ts-ignore
+import { Button } from "react-ui-lib";
+function App() {
+  return (
+    <>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
--- src/helpers.ts
+++
@@ -1,3 +0,0 @@
-export function printHello(): string {
-    return "Hello World!!!"
-}
